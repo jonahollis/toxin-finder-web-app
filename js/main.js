1462b774e60711a5fbc6cd8b112f7fb72f649600
Jonah Hollis <jonahquinnhollis@gmail.com>
2023-02-19 13:42:47 -0500

    implemented optimizations with regard to loading spinner, additional info presented, and info formatted into table

--- js/main.js
+++ js/main.js
@@ -1,36 +1,46 @@
 document.querySelector('#zipbutton').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  console.log(choice)
-  const url = `https://bypass-cors.j0n4h.repl.co/get?url=https://data.epa.gov/efservice/tri_facility/zip_code/BEGINNING/${choice}/JSON`
+  document.getElementById('spinner').innerHTML =	`<div class="d-flex justify-content-center m-5">
+  <div class="spinner-border" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+  </div>`
+
+  const zipcode = document.querySelector('input').value
+  console.log(zipcode)
+  const url = `https://bypass-cors.j0n4h.repl.co/get?url=https://data.epa.gov/efservice/tri_facility/zip_code/BEGINNING/${zipcode}/JSON`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
 
+        document.getElementById('spinner').innerHTML =	''
+
         document.querySelector('#result-header').innerHTML = `<h2>Results: ${data.length}</h2>`
 
         for(i = 0; i < data.length; i++){
-  
-          const API_Results = item => `<div id="single-result"><h3 id="label">Facility Name:</h3>
-          <h3 class="name">${item.FACILITY_NAME}</h3>
-          <h4 id="label">Parent Company Name:</h4> <h4 id="parent-co">${item.PARENT_CO_NAME}</h4><h4 id="label">Address:</h4> 
-          <h4 id="street">${item.MAIL_STREET_ADDRESS} </h4><h4 id="town">${item.MAIL_CITY}</h4><h4 id="zip-state">${item.MAIL_ZIP_CODE} ${item.MAIL_STATE_ABBR}</h4></div>`;
-  
-          document.querySelector("#results").innerHTML = data.map(item => API_Results(item)).join('');
 
+          let count = 1
+
+          const apiResults = item => 
+            `<tr>
+            <th scope="col"></th>
+            <td><b>${item.FACILITY_NAME}</b><br>EPA ID: ${item.EPA_REGISTRY_ID}</td>
+            <td>${item.PARENT_CO_NAME}</td>
+            <td>${item.MAIL_STREET_ADDRESS}, ${item.MAIL_CITY}, ${item.MAIL_ZIP_CODE} ${item.MAIL_STATE_ABBR}<br>PH: ${item.ASGN_PUBLIC_PHONE}</td></tr>`
+          
+            count++
+
+          document.querySelector("tbody").innerHTML = data.map(item => apiResults(item)).join('')
         }
         
-
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-
-
 
 
 
