bf1a029f918bf0db71e3e72f1d6b17321ef4b2f2
Jonah Hollis <jonahquinnhollis@gmail.com>
2023-02-21 09:53:35 -0500

    restructuring api request with async/await

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.querySelector('#zipbutton').addEventListener('click', getFetch)
 
-function getFetch(){
+async function getFetch(){
   document.getElementById('spinner').innerHTML =	`<div class="d-flex justify-content-center m-5">
   <div class="spinner-border" role="status">
     <span class="visually-hidden">Loading...</span>
@@ -11,7 +11,8 @@ function getFetch(){
   console.log(zipcode)
   const url = `https://bypass-cors.j0n4h.repl.co/get?url=https://data.epa.gov/efservice/tri_facility/zip_code/BEGINNING/${zipcode}/JSON`
 
-  fetch(url)
+  try {
+    await fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
@@ -30,17 +31,13 @@ function getFetch(){
             <td><b>${item.FACILITY_NAME}</b><br>EPA ID: ${item.EPA_REGISTRY_ID}</td>
             <td>${item.PARENT_CO_NAME}</td>
             <td>${item.MAIL_STREET_ADDRESS}, ${item.MAIL_CITY}, ${item.MAIL_ZIP_CODE} ${item.MAIL_STATE_ABBR}<br>PH: ${item.ASGN_PUBLIC_PHONE}</td></tr>`
-          
-            
 
           document.querySelector("tbody").innerHTML = data.map(item => apiResults(item)).join('')
         }
         
       })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+
+  }catch(error){
+    console.log(error)
+  }
 }
-
-
-
