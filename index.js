8b7c23613486445bb99c62bc4df1d63b9f80cc8d
J.Q. Hollis <jonahquinnhollis@gmail.com>
2024-01-19 23:03:45 -0500

    refactor api for xml

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser();
+const app = express();
 
-const app = express()
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-app.get(
-  '/get',
-  cors(),
-  async (req, res) => {
-    console.log(req.query)
-    // strip leading slash
-    const url = req.query.url
+app.get('/get', cors(), async (req, res) => {
+    console.log(req.query);
+    const url = req.query.url;
+    
     if (!url) {
-      res.send('url required')
-      return
+      res.status(400).send('URL required');
+      return;
     }
-    axios({
-      url,
-      method: 'GET',
-      responseType: 'stream',
-    }).then(data => {
-      data.data.pipe(res)
-    }).catch(err => {
-      console.log(`err on ${url}`, err.message)
-    })
-  },
-);
+
+    try {
+      const response = await axios.get(url, { responseType: 'text' });
+      parser.parseString(response.data, (err, result) => {
+        if (err) {
+          console.error('Error parsing XML:', err);
+          res.status(500).send('Error parsing XML');
+          return;
+        }
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        console.log(result);
+        res.json(result);
+      });
+    } catch (err) {
+      console.log(`Error on ${url}:`, err.message);
+      res.status(500).send('Server error occurred');
+    }
+});
 
 app.listen(8080, () => {
-  console.log('started')
-})
+  console.log('Server started on port 8080');
+});
